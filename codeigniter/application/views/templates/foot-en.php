aa86303898c33ea1f86f5aa2d2c882bf65a005a1
Tony Mao <tianguohut@163.com>
2015-12-01 21:54:36 +0800

    test

--- codeigniter/application/views/templates/foot-en.php
+++ codeigniter/application/views/templates/foot-en.php
@@ -1,4 +1,4 @@
-<script src="<?php echo $baseUrl; ?>libs/jquery/dist/jquery.min.js"></script>
+<script src="http://lib.sinaapp.com/js/jquery/2.0.3/jquery-2.0.3.min.js"></script>
 <script src="<?php echo $baseUrl; ?>libs/bootstrap/dist/js/bootstrap.min.js"></script>
 <script src="<?php echo $baseUrl; ?>js/main.min.js"></script>
 </body>
