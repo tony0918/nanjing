f24603cac8446b3c2b4091cf395e98c2417c0afc
Tony Mao <tianguohut@163.com>
2016-02-09 10:30:46 +0800

    Adding 2016 map

--- codeigniter/application/views/pages/map.php
+++ codeigniter/application/views/pages/map.php
@@ -12,29 +12,13 @@
       <h3 class="article-title text-center">2016汇添富南京山地马拉松赛事路线图</h3>
 
       <div class="text-block">
-        <h4 class="text-title">(一) 42公里组</h4>
-
         <p>
-          <img src="<?php echo $baseUrl; ?>assets/42_u57.jpg" alt="" class="img-responsive"/>
+          <a onclick="_hmt.push(['_trackEvent', 'img:map', 'click', 'map_2016.jpg','cn']);" href="<?php echo $baseUrl; ?>assets/map_2016_original.jpg" target="_blank">
+            <img src="<?php echo $baseUrl; ?>assets/map_2016.jpg" alt="" class="img-responsive"/>
+          </a>
         </p>
       </div>
-      <div class="text-block">
-        <h4 class="text-title">(二) 25公里组</h4>
 
-        <p>
-          <img src="<?php echo $baseUrl; ?>assets/25_u66.jpg" alt="" class="img-responsive"/>
-        </p>
-      </div>
-      
-      <div class="text-block">
-        <h4 class="text-title">(三) 12公里组: 地图有待上传</h4>
-
-<!--
-        <p>
-          <img src="<?php echo $baseUrl; ?>assets/25_u66.jpg" alt="" class="img-responsive"/>
-        </p>
--->
-      </div>
     </div>
   </div>
 </div>
