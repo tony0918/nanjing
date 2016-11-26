3b54052c7fc2f654db36be4da9389a64f828e84c
Tony Mao <tianguohut@163.com>
2016-11-26 23:46:33 +0800

    change footer 1

--- codeigniter/application/views/templates/footer.php
+++ codeigniter/application/views/templates/footer.php
@@ -1,14 +1,14 @@
 <footer class="container-fluid">
   <div class="row main-footer">
 
-    <div class="col-md-3 col-md-offset-2 col-xs-12 text-center">
+    <div class="col-md-4 col-xs-12">
       <p>主办单位：</p>
       <p>南京市户外运动协会</p>
       <p>南京市江宁区体育总会</p>
       <p>上海穹景体育文化发展有限公司</p>
     </div>
 
-    <div class="col-md-2 col-xs-12 text-center">
+    <div class="col-md-4 col-xs-12 text-center">
       <p>
         <label for="language">Language：</label>
         <select name="" id="language">
@@ -19,8 +19,22 @@
       <p>沪ICP备14023412号-2</p>
     </div>
 
-    <div class="col-md-3 col-xs-12 text-center">
-      <span class="hidden-xs">关注我们：</span><img src="<?php echo $baseUrl;?>images/qr.jpg" alt="">
+    <div id="shareBar" class="clearfix">
+      <div class="tit pull-left">关注我们</div>
+      <ul class="shareList pull-left">
+        <li><a href="" class="sina">sina</a></li>
+        <li><a href="" class="wechat">wechat</a></li>
+        <li><a href="" class="qrcode">qrcode</a></li>
+      </ul>
+    </div>
+
+    <div class="col-md-4 hidden-xs text-right">
+      <div class="tit pull-left">关注我们</div>
+      <ul class="shareList pull-left">
+        <li><a href="http://weibo.com/u/3239397594" target="_blank" class="sina">sina</a></li>
+        <li><a href="" class="wechat">wechat</a></li>
+        <li><img src="<?php echo $baseUrl; ?>images/qr.jpg" alt=""></li>
+      </ul>
     </div>
 
   </div>
