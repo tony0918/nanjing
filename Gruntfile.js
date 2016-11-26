a78252dd648e6eb1ce634db1c2089199bc40fc1c
Tony Mao <tianguohut@163.com>
2016-11-27 01:05:48 +0800

    Change style color

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,147 +1,153 @@
 module.exports = function (grunt) {
-  'use strict';
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        //        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      production: {
-        src: 'src/js/*.js',
-        dest: 'dist/js/main.min.js'
-      }
-    },
-    watch: {
-      files: ['src/less/**/*.less'],
-      tasks: ['less', 'autoprefixer']
-    },
-    less: {
-      development: {
-        options: {
-          paths: ["src/less/parts"]
-        },
-        files: {
-          'dist/css/main.css': 'src/less/main.less'
-        }
-      },
-      production: {
-        options: {
-          paths: ["src/less/parts"]
-        },
-        files: {
-          'tmp/main.css': 'src/less/main.less'
-        }
-      }
-    },
-    copy: {
-      development: {
-        files: [
-          {
-            expand: true,
-            cwd: 'src',
-            src: ['libs/**'],
-            dest: 'dist/'
-          }
-        ]
-      },
-      production: {
-        files: [
-          {
-            expand: true,
-            cwd: 'src',
-            src: ['libs/**'],
-            dest: 'dist/'
-          }, {
-            expand: true,
-            cwd: 'bower_components',
-            src: ['**/{dist,dest}/**/*.min.{js,css}', '**/dist/**/fonts/*', '!**/src/**/*'],
-            dest: 'dist/libs'
-          }
-        ]
-      }
-    },
-    autoprefixer: {
-      options: {
-        browsers: ['last 8 versions']
-      },
-      production: {
-        src: 'tmp/main.css'
-      },
-      development: {
-        src: 'dist/css/main.css'
-      }
-    },
-    clean: {
-      development: ['dist', 'tmp']
-    },
-    cssmin: {
-      production: {
-        files: {
-          'dist/css/main.css': 'tmp/main.css'
-        }
-      }
-    },
-    imagemin: {
-      production: {
-        files: [
-          {
-            expand: true,
-            cwd: 'src/',
-            src: ['images/**/*.{png,jpg,gif}', 'assets/**/*.{png,jpg,gif}'],
-            dest: 'dist'
-          }
-        ]
-      }
-    },
-    cdnify: {
-      production: {
-        options: {
-          rewriter: function (url) {
-            if (url.indexOf('../bower_components/') === 0) {
-              return url.replace('../bower_components/', 'libs/');
-            } else if (url.indexOf('../dist/') === 0) {
-              return url.replace('../dist/', '');
-            } else {
-              return url;
+    'use strict';
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                //        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            production: {
+                src: 'src/js/*.js',
+                dest: 'dist/js/main.min.js'
             }
-          },
-          html: {
-            'img[data-src]': false,
-            'img[src]': false,
-            'link[rel="]': false,
-            'link[rel="shortcut icon"]': 'href',
-            'link[rel=icon]': 'href',
-            'link[rel=stylesheet]': 'href',
-            'script[src]': 'src',
-            'source[src]': 'src',
-            'video[poster]': false
-          }
         },
-        files: [{
-          src: ['*.html'],
-          dest: 'dist',
-          expand: true,
-          cwd: 'src'
-        }]
-      }
-    }
-  });
+        watch: {
+            files: ['src/less/**/*.less'],
+            tasks: ['less', 'autoprefixer']
+        },
+        less: {
+            development: {
+                options: {
+                    paths: ["src/less/parts"]
+                },
+                files: {
+                    'dist/css/main.css': 'src/less/main.less'
+                }
+            },
+            production: {
+                options: {
+                    paths: ["src/less/parts"]
+                },
+                files: {
+                    'tmp/main.css': 'src/less/main.less'
+                }
+            }
+        },
+        copy: {
+            development: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src',
+                        src: ['libs/**'],
+                        dest: 'dist/'
+                    }
+                ]
+            },
+            production: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src',
+                        src: ['libs/**'],
+                        dest: 'dist/'
+                    }, {
+                        expand: true,
+                        cwd: 'bower_components',
+                        src: ['**/{dist,dest}/**/*.min.{js,css}', '**/dist/**/fonts/*', '!**/src/**/*'],
+                        dest: 'dist/libs'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'src/images',
+                        src: ['**/*.svg'],
+                        dest: 'dist/images'
+                    }
+                ]
+            }
+        },
+        autoprefixer: {
+            options: {
+                browsers: ['last 8 versions']
+            },
+            production: {
+                src: 'tmp/main.css'
+            },
+            development: {
+                src: 'dist/css/main.css'
+            }
+        },
+        clean: {
+            development: ['dist', 'tmp']
+        },
+        cssmin: {
+            production: {
+                files: {
+                    'dist/css/main.css': 'tmp/main.css'
+                }
+            }
+        },
+        imagemin: {
+            production: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src/',
+                        src: ['images/**/*.{png,jpg,gif}', 'assets/**/*.{png,jpg,gif}'],
+                        dest: 'dist'
+                    }
+                ]
+            }
+        },
+        cdnify: {
+            production: {
+                options: {
+                    rewriter: function (url) {
+                        if (url.indexOf('../bower_components/') === 0) {
+                            return url.replace('../bower_components/', 'libs/');
+                        } else if (url.indexOf('../dist/') === 0) {
+                            return url.replace('../dist/', '');
+                        } else {
+                            return url;
+                        }
+                    },
+                    html: {
+                        'img[data-src]': false,
+                        'img[src]': false,
+                        'link[rel="]': false,
+                        'link[rel="shortcut icon"]': 'href',
+                        'link[rel=icon]': 'href',
+                        'link[rel=stylesheet]': 'href',
+                        'script[src]': 'src',
+                        'source[src]': 'src',
+                        'video[poster]': false
+                    }
+                },
+                files: [{
+                    src: ['*.html'],
+                    dest: 'dist',
+                    expand: true,
+                    cwd: 'src'
+                }]
+            }
+        }
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-newer');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-cdnify');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-newer');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-cdnify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Default task(s).
-  grunt.registerTask('default', ['clean', 'less:development', 'autoprefixer:development', 'copy:development', 'watch']);
-  grunt.registerTask('imgmin', ['newer:imagemin']);
-  grunt.registerTask('deploy', ['clean', 'less:production', 'autoprefixer:production', 'uglify', 'cssmin', 'copy:production', 'newer:imagemin', 'cdnify']);
+    // Default task(s).
+    grunt.registerTask('default', ['clean', 'less:development', 'autoprefixer:development', 'copy:development', 'watch']);
+    grunt.registerTask('imgmin', ['newer:imagemin']);
+    grunt.registerTask('deploy', ['clean', 'less:production', 'autoprefixer:production', 'uglify', 'cssmin', 'copy:production', 'newer:imagemin', 'cdnify']);
 
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,42 +1,42 @@
 (function ($) {
-  'use strict';
-  $(function () {
-    function countDown(time, id) {
-      var day_elem = $(id).find('#t_h');
-      var hour_elem = $(id).find('#t_m');
-      var minute_elem = $(id).find('#t_s');
-      var second_elem = $(id).find('.second');
-      var end_time = new Date(time).getTime(), //月份是实际月份-1
-        sys_second = (end_time - new Date().getTime()) / 1000;
-      var timer = setInterval(function () {
-        if (sys_second > 1) {
-          sys_second -= 1;
-          var day = Math.floor((sys_second / 3600) / 24);
-          var hour = Math.floor((sys_second / 3600) % 24);
-          var minute = Math.floor((sys_second / 60) % 60);
-          var second = Math.floor(sys_second % 60);
-          day_elem && $(day_elem).text(day); //计算天
-          $(hour_elem).text(hour < 10 ? "0" + hour : hour); //计算小时
-          $(minute_elem).text(minute < 10 ? "0" + minute : minute); //计算分钟
-          $(second_elem).text(second < 10 ? "0" + second : second); //计算秒杀
-        } else {
-          clearInterval(timer);
+    'use strict';
+    $(function () {
+        function countDown(time, id) {
+            var day_elem = $(id).find('#t_h');
+            var hour_elem = $(id).find('#t_m');
+            var minute_elem = $(id).find('#t_s');
+            var second_elem = $(id).find('.second');
+            var end_time = new Date(time).getTime(), //月份是实际月份-1
+                sys_second = (end_time - new Date().getTime()) / 1000;
+            var timer = setInterval(function () {
+                if (sys_second > 1) {
+                    sys_second -= 1;
+                    var day = Math.floor((sys_second / 3600) / 24);
+                    var hour = Math.floor((sys_second / 3600) % 24);
+                    var minute = Math.floor((sys_second / 60) % 60);
+                    var second = Math.floor(sys_second % 60);
+                    day_elem && $(day_elem).text(day); //计算天
+                    $(hour_elem).text(hour < 10 ? "0" + hour : hour); //计算小时
+                    $(minute_elem).text(minute < 10 ? "0" + minute : minute); //计算分钟
+                    $(second_elem).text(second < 10 ? "0" + second : second); //计算秒杀
+                } else {
+                    clearInterval(timer);
+                }
+            }, 1000);
         }
-      }, 1000);
-    }
 
-    if ($('.time_area').attr('data-time')) {
-      countDown($('.time_area').attr('data-time'), ".time_area");
-    }
+        if ($('.time_area').attr('data-time')) {
+            countDown($('.time_area').attr('data-time'), ".time_area");
+        }
 
-    $('#language').change(function () {
-      var v = $(this).children('option:selected').val();
-      if ('en' == v) {
-        window.location.href = '/en';
-      } else {
-        window.location.href = '/';
-      }
+        // $('#language').change(function () {
+        //     var v = $(this).children('option:selected').val();
+        //     if ('en' == v) {
+        //         window.location.href = '/en';
+        //     } else {
+        //         window.location.href = '/';
+        //     }
+        // });
     });
-  });
 
 })(jQuery);
