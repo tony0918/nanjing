9412256fa6b4e6bbde029bee6fdd51a84df78784
Tony Mao <tianguohut@163.com>
2017-03-27 11:28:22 +0800

    Update photos

--- codeigniter/application/views/pages/photos.php
+++ codeigniter/application/views/pages/photos.php
@@ -1,7 +1,7 @@
 <div class="container-fluid">
   <div class="row header-banner">
     <img src="<?php echo $baseUrl; ?>images/header-banner.jpg" alt="" class="img-responsive"/>
-<!--    <h4 class="page-title hidden-xs text-center col-md-12">照片下载</h4>-->
+    <!--    <h4 class="page-title hidden-xs text-center col-md-12">照片下载</h4>-->
   </div>
 </div>
 
@@ -13,32 +13,34 @@
   </div>
   <div class="row">
     <p class="col-xs-10 col-xs-offset-1 col-md-2 col-md-offset-3">
-      <img src="<?php echo $baseUrl; ?>assets/small-kv1.jpg" alt="" class="img-responsive"/>
+      <img src="<?php echo $baseUrl; ?>assets/photo1.jpg" alt="" class="img-responsive"/>
     </p>
 
     <p class="col-xs-10 col-xs-offset-1 col-md-2 col-md-offset-0">
-      <img src="<?php echo $baseUrl; ?>assets/small-kv1.jpg" alt="" class="img-responsive"/>
+      <img src="<?php echo $baseUrl; ?>assets/photo2.jpg" alt="" class="img-responsive"/>
     </p>
 
     <p class="col-xs-10 col-xs-offset-1 col-md-2 col-md-offset-0">
-      <img src="<?php echo $baseUrl; ?>assets/small-kv1.jpg" alt="" class="img-responsive"/>
+      <img src="<?php echo $baseUrl; ?>assets/photo3.jpg" alt="" class="img-responsive"/>
     </p>
     <p class="col-xs-10 col-xs-offset-1 col-md-2 col-md-offset-3">
-      <img src="<?php echo $baseUrl; ?>assets/small-kv1.jpg" alt="" class="img-responsive"/>
+      <img src="<?php echo $baseUrl; ?>assets/photo4.jpg" alt="" class="img-responsive"/>
     </p>
 
     <p class="col-xs-10 col-xs-offset-1 col-md-2 col-md-offset-0">
-      <img src="<?php echo $baseUrl; ?>assets/small-kv1.jpg" alt="" class="img-responsive"/>
+      <img src="<?php echo $baseUrl; ?>assets/photo5.jpg" alt="" class="img-responsive"/>
     </p>
 
     <p class="col-xs-10 col-xs-offset-1 col-md-2 col-md-offset-0">
-      <img src="<?php echo $baseUrl; ?>assets/small-kv1.jpg" alt="" class="img-responsive"/>
+      <img src="<?php echo $baseUrl; ?>assets/photo6.jpg" alt="" class="img-responsive"/>
     </p>
   </div>
   <div class="row">
     <div class="col-sm-12 col-md-8 col-md-offset-2">
       <div class="text-center">
-        <p><a class="btn btn-default btn-lg" href="">查看更多</a></p>
+        <p>
+          <a class="btn btn-default btn-lg" href="http://iranshao.com/races/786/albums?year=2017" target="_blank">查看更多</a>
+        </p>
       </div>
     </div>
   </div>
