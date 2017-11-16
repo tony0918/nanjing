7fe69fc0324c685c1c123d9670cee1263a95f541
Tony Mao <tmao@nvidia.com>
2017-11-16 23:45:01 +0800

    update guidelines-en for 2018

--- codeigniter/application/views/pages/guidelines-en.php
+++ codeigniter/application/views/pages/guidelines-en.php
@@ -8,14 +8,18 @@
 <div class="container">
   <div class="row">
     <div class="col-sm-12 col-md-8 col-md-offset-2">
-      <h3 class="article-title text-center">Registration Guidelines of 2017 CUAM Nanjing Mountain Marathon</h3>
+      <h3 class="article-title text-center">Registration Guidelines of 2017 CUAM
+        Nanjing Mountain Marathon</h3>
       <div class="text-block">
         <h4 class="text-title">I. Race date and events</h4>
-        <p>1. Race date: March 26th, 2017 (Sunday)</p>
+        <p>1. Race date: April 1st, 2018 (Sunday)</p>
         <p>2. Events</p>
-        <p style="padding-left: 10px;">2.1 42km group: with an elevation gain of 1200m, 350 participants</p>
-        <p style="padding-left: 10px;">2.2 25km group: with an elevation gain of 500m, 600 participants</p>
-        <p style="padding-left: 10px;">2.3 12km Family group: with an elevation gain of 300m, 50 groups</p>
+        <p style="padding-left: 10px;">2.1 42km group: with an elevation gain of
+          1200m, 400 participants</p>
+        <p style="padding-left: 10px;">2.2 25km group: with an elevation gain of
+          500m, 600 participants</p>
+        <p style="padding-left: 10px;">2.3 12km YOUNG ATHLETE group: with an
+          elevation gain of 300m, 100 groups</p>
       </div>
       <div class="text-block">
         <h4 class="text-title">II. Qualification</h4>
@@ -24,32 +28,40 @@
           physical checks, can register the race with no regard of
           nationalities.</p>
         <p>2. Age limits:</p>
-        <p style="padding-left: 10px;">2.1 Participants in the 42km and 25km group must be at least 18 years old (born
-          before March 26, 1999),
-          in a good health condition, and shall be runners with regular training.</p>
-        <p style="padding-left: 10px;">2.2 Each team in the 12km family will be formed by an adolescent between 8 years
-          old and 18 years old and a
-          parent or a legal guardian. They should enroll the race together. Anyone in a good health condition can
-          participates in the race.</p>
-        <p style="padding-left: 10px;">Remarks: Adolescent participants between 8 and 18 years old can only enroll the
-          race with a statement signed
-          by their guardians or legal representatives.</p>
-        <p>3. Patients of the following diseases are not in suitable health conditions for the race.</p>
-        <p style="padding-left: 10px;">3.1 Congenital heart disease and rheumatic heart disease.</p>
-        <p style="padding-left: 10px;">3.2 Hypertension and cerebrovascular disease</p>
-        <p style="padding-left: 10px;">3.3 Myocarditis and other heart diseases</p>
-        <p style="padding-left: 10px;">3.4 Coronary artery disease and server irregular heartbeat</p>
+        <p style="padding-left: 10px;">2.1 Participants in the 42km and 25km
+          group must be at least 18 years old (born before April 1st, 2000), in
+          a good health condition, and shall be runners with regular
+          training.</p>
+        <p style="padding-left: 10px;">2.2 Each team in the 12km YOUNG ATHLETE
+          group will be formed by an adolescent between 8 years old and 18 years
+          old and a parent or a legal guardian. They should enroll the race
+          together. Anyone in a good health condition can participates in the
+          race.</p>
+        <p style="padding-left: 10px;">Remarks: Adolescent participants between
+          8 and 18 years old can only enroll the race with a statement signed by
+          their guardians or legal representatives.</p>
+        <p>3. Patients of the following diseases are not in suitable health
+          conditions for the race.</p>
+        <p style="padding-left: 10px;">3.1 Congenital heart disease and
+          rheumatic heart disease.</p>
+        <p style="padding-left: 10px;">3.2 Hypertension and cerebrovascular
+          disease</p>
+        <p style="padding-left: 10px;">3.3 Myocarditis and other heart
+          diseases</p>
+        <p style="padding-left: 10px;">3.4 Coronary artery disease and server
+          irregular heartbeat</p>
         <p style="padding-left: 10px;">3.5 Diabetes</p>
-        <p style="padding-left: 10px;">3.6 Other diseases that is not suitable for sports.</p>
+        <p style="padding-left: 10px;">3.6 Other diseases that is not suitable
+          for sports.</p>
       </div>
       <div class="text-block">
         <h4 class="text-title">III. Open period for registration</h4>
-        <p>December 15th, 9:00,2016 – February 15th, 17:00,2017</p>
+        <p>November 28th, 10:00,2017 – February 24th, 17:00,2018</p>
         <p>All registration is based on a first-come-first-served basis.</p>
       </div>
       <div class="text-block">
         <h4 class="text-title">IV. Registration</h4>
-        <p>Log onto the official website of 2017 CUAM Nanjing Mountain Marathon
+        <p>Log onto the official website of 2018 CUAM Nanjing Mountain Marathon
           at <a href="http://nanjing.huluanpao.com">nanjing.huluanpao.com</a> to
           register online.</p>
       </div>
@@ -72,23 +84,46 @@
             <td>350 RMB per head</td>
           </tr>
           <tr>
-            <td>12km Family group</td>
+            <td>12km YOUNG ATHLETE group</td>
             <td>200 RMB per head</td>
+          </tr>
+          <tr>
+            <td>42km group - Team Registration</td>
+            <td>2000 RMB per team</td>
+          </tr>
+          <tr>
+            <td>25km group - Team Registration</td>
+            <td>1200 RMB per team</td>
           </tr>
           </tbody>
         </table>
-        <p>Remark: The whole entry fee 200 yuan for the 12km family group will be donated to the charity project in
-          cooperation with the race: River and Kids.. Additional donations from the runners are also encouraged.</p>
+        <p>Remark: </p>
+        <p>1) The whole entry fee of 12km Junior Group, 200 RMB per person,
+          will be donated to the partner charity project of the race, River &
+          Kids. We also encourage donation by means of purchasing the charity
+          goody bags. (60 RMB per goody bag, available for purchasing at
+          registration)</p>
+        <p>2) There will be a discount for team registration in 42km and 25km
+          group. Only limited to 50 teams in either group.</p>
+        <p>3) The individual result of each team member in 42km and 25km group
+          will be the result recorded by his/her timing chip. The team’s time
+          will be calculated by adding all four members’ individual time and
+          this result will decide the team’s ranking. The four team members
+          won’t have to arrive at the finish line at the same time.</p>
+        <p>4) After the race a top 10 team list will be published with their
+          respective team time on it. All teams will be given a race certificate
+          on the spot. There will be no awarding ceremony.</p>
       </div>
       <div class="text-block">
         <h4 class="text-title">VI. Timing Chip</h4>
         <p>All events will adopt the disposal timing chip. Runners will have no
           need to return the timing chips after the race.(The parent or legal
-          guardian of 12km family team has no timing chips)</p>
+          guardian of 12km YOUNG ATHLETE group has no timing chips)</p>
       </div>
       <div class="text-block">
-        <h4 class="text-title">VII. The online registration process and things to note</h4>
-        <p>1. Log onto the official website of 2016 CUAM Nanjing Mountain
+        <h4 class="text-title">VII. The online registration process and things
+          to note</h4>
+        <p>1. Log onto the official website of 2018 CUAM Nanjing Mountain
           Marathon at <a href="http://nanjing.huluanpao.com">nanjing.huluanpao.com</a>
           to register online.</p>
         <p>2. Read the registration notes, the insurance instruction and related
@@ -96,12 +131,21 @@
         <p>3. Fill in the registration form.</p>
         <p>4. Pay entry fee online. (Alipay/Wechat and credit card are
           supported.)</p>
-        <p>5. The online registration will be completed after the pay success.</p>
-        <p>6. Participants who have successfully registered need to pick up their race packets with race packet pickup
-          certificate and original ID at Crowne Plaza ( No.9 East Jiahu Road, Jiangning District, Nanjing ) from 10 : 00
-          am to 20 : 00 pm on March 25th ( Saturday ). Please check you BIB number and download the race packet pickup
-          certificate from official website from Mar 1st,2017 to Mar 25th,2017 .You won’t be allowed to pick up race
-          packets on behalf of others.</p>
+        <p>5. The online registration will be completed after the pay
+          success.</p>
+        <p>6. Participants who have successfully registered need to pick up
+          their race packets with race packet pickup certificate and original ID
+          at Crowne Plaza ( No.9 East Jiahu Road, Jiangning District, Nanjing )
+          from 10:00 am to 20:00 pm on March 31th ( Saturday ). Please check
+          you BIB number and download the race packet pickup certificate from
+          official website from Mar 6st,2018 to Mar 31th,2018 . In the case of
+          withdrawal from the race, you can either leave the pickup of the race
+          packet to a friend or email to service@huluanpao.com within five
+          working days after the race to apply for the mail of the race packet.
+          In the first case, your friend will be able to pick up the race gear
+          other than the timing chip and the bib number for you by showing the
+          photocopy of your ID. In the second case, the gear will be sent to you
+          by express C.O.D.</p>
       </div>
       <div class="text-block">
         <h4 class="text-title">VIII. After the registration is confirmed, you
@@ -114,9 +158,8 @@
           final interpretation of this "registration notes". All pending events
           will be given further notice.</h4>
       </div>
-      <p class="text-right">Committee of the 2017 CUAM Nanjing Mountain
-        Marathon</p>
-      <p class="text-right">November, 2016</p>
+      <p class="text-right">Committee of the 2018 CUAM Nanjing Mountain Marathon</p>
+      <p class="text-right">November, 2017</p>
     </div>
   </div>
 </div>
