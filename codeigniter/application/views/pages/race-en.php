242c5f1206f490d2e29fbb070fddeacbb6e3fdc0
Tony Mao <tmao@nvidia.com>
2017-11-22 14:16:41 +0800

    Update race page

--- codeigniter/application/views/pages/race-en.php
+++ codeigniter/application/views/pages/race-en.php
@@ -11,62 +11,91 @@
       <h3 class="article-title text-center">CUAM Nanjing Mountain Marathon</h3>
 
       <div class="text-block">
-        <h4 class="text-title">2017 CUAM Nanjing Mountain Marathon</h4>
-        <p>Through trail running, one of the outdoor activities, 2017 CUAM Nanjing Mountain Marathon is able to spread
-          such a spirit with which human beings keep challenging their own limits, and offers the race participants an
-          opportunity to discover and enjoy the cultural and natural beauty of Nanjing. With mountains, trails and woods
-          all combined in the same course, the runners will be fully enjoying the elegant landscapes of Jiangnan, the
-          regions in the south of Yangtze River, as they run along the Panlong Lake. 2017 CUAM Nanjing Mountain Marathon
-          will be the fourth Nanjing Mountain Marathon.</p>
+        <h4 class="text-title">2018 CUAM Nanjing Mountain Marathon</h4>
+        <p>This classic entry-level trail race is dedicated to spread through
+          trail running the challenge spirit with which the human beings keep
+          pushing the limits. It also offers the participants an opportunity to
+          discover and enjoy the cultural and natural beauty of Nanjing. With
+          mountain tracks, trails and woods all combined in the same course, the
+          runners will be fully enjoying the elegant landscapes of Jiangnan, the
+          regions in the south of Yangtze River, as they run along the Panlong
+          Lake.</p>
+        <p>At the same time, it recorded tons of precious moments of every
+          participant in every edition of the race in the past five years, the
+          tenderness of a lonely runner being expected at the finish line, the
+          friendship of two runners’ mutual support and crossing the finish line
+          hand in hand, the family affection of the kid accompanied by the
+          parent in his/her first ever trail race, etc…</p>
+        <p>2018 CUAM Nanjing Mountain Marathon will be the race’s fifth edition.
+          We expect to see you on the fifth anniversary of the race.</p>
       </div>
       <div class="text-block">
         <h4 class="text-title">About Nanjing</h4>
-        <p>Located in the eastern area of China, Nanjing is the capital of Jiangsu Province and was the capital of
-          multiple ancient Chinese Dynasties. It has always been in an important position in China's history and
-          culture. Situated in the lower reaches of the Yangtze River as well as in the Yangtze River Delta Economic
-          Zone, Nanjing is one of the most important cities in China. It's one of the four greatest ancient capitals in
-          China. The state of Wu, one of the three kingdoms, once set up its capital here. It's quite possible that
-          you'll feel like keeping running and keeping racing even after the race ends.</p>
+        <p>Nanjing is the capital of east China’s Jiangsu Province. It was the
+          capital of various ancient Chinese dynasties and forms an important
+          part of the Chinese history and culture. Located in the lower basin of
+          Yangtze River and the Yangtze River Delta economic zone, it’s one of
+          the most important cities in China. It was one of the four great
+          ancient capitals of China. For example, in the Three Kingdom’s period,
+          Wu State established its capital here. Maybe in the race you would
+          gradually indulge yourself in the historic atmosphere of Nanjing and
+          be inspired to race stronger.</p>
       </div>
       <div class="text-block">
         <h4 class="text-title">About “River·Kids”</h4>
-        <p>"Wherever there are rivers there are villages. Wherever there are villages there are kids. Wherever there
-          are kids there is hope."</p>
-        <p>With the intention of "train a batch of country teachers, construct a school with donations and help a
-          batch of students in order to change the current education situation in the countrysides of the western part
-          of China", the charity plan "River Kids" has been mainly focusing on training teachers and has been
-          investing hardware as well as a supplement. By adopting such an operation mode that combines spiritual and
-          material investments, the plan aims to not only enhance the overall education level of the impoverished
-          regions, but also have a long-term impact on generations and generations of country students, and launch a
-          revolution in ideas at root in these regions in the end.</p>
-        <p>Thousands of volunteers have been working together in the past 9 years to launch "River·Kids" plan in
-          Lushui County of Yunnan Province, Meigu County of Sichuan Province, Jingyuan County of Ningxia Province,
-          Xiahe County of Gansu Province, Liping County of Guizhou Province, Zhonglu Town of Hubei Province, Huzhu
-          County of Qinghai province, Yongle Town of Guangxi Province and Chengbu County of Hunan Province. With the
-          received donations, 9 well-equipped Tianfu Elementary Schools have been constructed in these areas and more
-          than 3000 poor students from rural areas and more than 700 country teachers have been helped. These students
-          and teachers are from a total of 9 minorities, including Lisu, Yi, Hui, Tibetan, Dong, Tujia, Tu, Yao and
-          Miao.</p>
-        <p>After many years' successful project running, "River Kids" have formed an education aiding system, covering
-          almost all areas, including sub-projects such as training for country youngster teachers, donation and
-          construction for Tianfu Elementary Schools, donation for library rooms, multi-media classrooms, student
-          grants, faculty fellowship and school and living supplies, and organizations of charity trips.</p>
+        <p>"Wherever there are rivers there are villages. Wherever there are
+          villages there are kids. Wherever there are kids there is hope."</p>
+        <p>With the intention of "train a batch of country teachers, construct a
+          school with donations and help a batch of students in order to change
+          the current education situation in the countrysides of the western
+          part of China", the charity plan "River Kids" has been mainly focusing
+          on training teachers and has been investing hardware as well as a
+          supplement. By adopting such an operation mode that combines spiritual
+          and material investments, the plan aims to not only enhance the
+          overall education level of the impoverished regions, but also have a
+          long-term impact on generations and generations of country students,
+          and launch a revolution in ideas at root in these regions in the
+          end.</p>
+        <p>Thousands of volunteers have been working together in the past 10
+          years to launch "River·Kids" plan in Lushui County of Yunnan Province,
+          Meigu County of Sichuan Province, Jingyuan County of Ningxia Province,
+          Xiahe County of Gansu Province, Liping County of Guizhou Province,
+          Zhonglu Town of Hubei Province, Huzhu County of Qinghai Province,
+          Yongle Town of Guangxi Province, Chengbu County of Hunan Province, and
+          Moguqi Town of Inner Mongolia. With the received donations, 10
+          well-equipped Tianfu Elementary Schools have been constructed in these
+          areas and more than 4000 poor students from rural areas and more than
+          800 country teachers have been helped. These students and teachers are
+          from a total of 10 minorities, including Lisu, Yi, Hui, Tibetan, Dong,
+          Tujia, Tu, Yao, Miao and Mongol Nationality.</p>
+        <p>After many years' successful project running, "River Kids" have
+          formed an education aiding system, covering almost all areas,
+          including sub-projects such as training for country youngster
+          teachers, donation and construction for Tianfu Elementary Schools,
+          donation for library rooms, multi-media classrooms, student grants,
+          faculty fellowship and school and living supplies, and organizations
+          of charity trips.</p>
       </div>
       <div class="text-block">
         <h4 class="text-title">About CUAM</h4>
-        <p>China Universal Asset Management Co., ltd, one of the most outstanding and most energetic asset managers in
-          China, once again becomes the title sponsor of our race.</p>
-        <p>CUAM was founded in 2004. With a strong creativity to lead the market, it has publicized multiple innovative
-          products in the industry and was awarded the first prize of the Financial Innovation Prize of Shanghai in
-          2011. It's one of the first asset management companies who won the qualification of QDII, one of the first who
-          was granted to establish oversea subsidiary corporations, and also one of the first who got the qualification
-          of RQFII. It has steadily developed to be one of the best asset management companies in China, and has become
-          one of China's asset management companies who are equipped with the most prominent teams in the industry, who
-          enjoy the brands with the most powerful influences and who have achieved the most steady and the most
-          remarkable performances.</p>
-        <p>Official website of CUAM: www.99fund.com; Customer service: 400 888 9918; Wechat: htffund</p>
-        <p>Conquer all targets under the morning sun. Release your motive power in company with the mountains and the
-          lake.</p>
+        <p>China Universal Asset Management Co., ltd, one of the most
+          outstanding and most energetic asset managers in China, is the
+          co-founder and the title sponsor of our race.</p>
+        <p>China Universal was founded in 2005. It’s one of the leading
+          comprehensive asset management companies in China. China Universal has
+          formed six business areas, including mutual funds, private equity,
+          internet finance, pension management, international business and
+          segregated account management. It earned its reputation of expert in
+          stock selection and won the recognition and trust of the fund holders
+          and domestic and abroad institutions. Since established, China
+          Universal has received awards many times, including Golden Bull Award
+          of China's Fund Industry, China's Star Fund Award, China’s Golden Fund
+          Award, Shanghai Financial Innovation First Prize and many other top
+          honors. China Universal always adheres to the principle Clients First
+          and the management idea Adopt a Long Term Perspective, and aims to
+          become the most recognized asset management brand in China.</p>
+        <p>Official website of CUAM: www.99fund.com Customer service: 400 888
+          9918 Wechat: htffund</p>
       </div>
     </div>
   </div>
